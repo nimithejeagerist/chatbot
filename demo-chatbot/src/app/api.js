d32d6a044eade0717cc5f17460c00a8cada0a5b9
Nimi <takinroy@ualberta.ca>
2025-11-27 17:21:42 -0700

    bug fixes

--- demo-chatbot/src/app/api.js
+++ demo-chatbot/src/app/api.js
@@ -5,29 +5,24 @@ Also, avoid lengthy answers and any bold words or italics.
 `;
 
 export async function sendToAI(chatHistory) {
-    const apiKey = process.env.NEXT_PUBLIC_OPENAI_API_KEY;
-    
-    // Use try/catch block, we don't know if this will always work
-    try {
-        const response = await fetch("https://api.openai.com/v1/chat/completions", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                Authorization: `Bearer ${apiKey}`
-            },
-            body: JSON.stringify({
-                model: "gpt-4o-mini",
-                messages: [
-                    {role: "system", content: SYSTEM_PROMPT},
-                    ...chatHistory
-                ]
-            })
-        });
-    }
-    catch (error) {
-        console.log(`Error: ${error.response}`);
-    }
+  const apiKey = process.env.NEXT_PUBLIC_OPENAI_API_KEY;
 
-    const data = await response.json();
-    return data.choices[0].message.content;
+  // Use try/catch block, we don't know if this will always work
+
+   const response = await fetch("https://api.openai.com/v1/chat/completions", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${apiKey}`,
+    },
+    body: JSON.stringify({
+      model: "gpt-4o-mini",
+      messages: [{ role: "system", content: SYSTEM_PROMPT }, ...chatHistory],
+    }),
+  });
+
+  console.log(response);
+
+  const data = await response.json();
+  return data.choices[0].message.content;
 }
